673b176d27bda107f6e50d57cc695d10a529bda9
DJ Roychoudhury and Tommy Jones <dj@>
2016-04-18 15:33:50 -0700

    test passed for first test.

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -1,7 +1,31 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+
 public class PingPong {
   public static void main(String[] args) {}
 
-  public boolean isPingPong(Integer num) {
-    return true;
+  public static ArrayList<Object> isPingPong(Integer num) {
+    ArrayList<Object> pingPongArray = new ArrayList<Object>();
+    String ping = "ping";
+    String pong = "pong";
+    String pingpong = "pingpong";
+    for (Integer index = 1; index <= num; index++)
+     {
+      if(index % 15 == 0){
+        pingPongArray.add(pingpong);
+      } else if (index % 5 == 0){
+        pingPongArray.add(pong);
+      } else if (index % 3 == 0){
+        pingPongArray.add(ping);
+      } else {
+        pingPongArray.add(index);
+      }
+    }
+    return pingPongArray;
   }
 }
--- src/test/java/PingPongTest.java
+++ src/test/java/PingPongTest.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import org.junit.*;
 import static org.junit.Assert.*;
 
@@ -5,45 +6,85 @@ public class PingPongTest {
   @Test
   public void isPingPong_countUpToGivenNumber_Array() {
     PingPong testPingPong = new PingPong();
-    ArrayList<Integer> expected = new ArrayList<Integer>();
+    ArrayList<Object> expected = new ArrayList<Object>();
     expected.add(1);
     expected.add(2);
-    assertEquals(expected, testPingPong.pingPong(2));
+    assertEquals(expected, testPingPong.isPingPong(2));
   }
-
-  @Test
-  public void isPingPong_forNumberLessThanThree_true() {
-    PingPong testPingPong = new PingPong();
-    assertEquals(true, pingPong.isPingPong(2))
-  }
-  @Test
-  public void isPingPong_forNumbersDivisibleByThree_true() {
-    PingPong testPingPong = new PingPong();
-    assertEquals(true, pingPong.isPingPong(3))
-  }
-  @Test
-  public void isPingPong_forNumbersNotDivisibleByThree_false() {
-    PingPong testPingPong = new PingPong();
-    assertEquals(false, pingPong.isPingPong(7))
-  }
-  @Test
-  public void isPingPong_forNumbersDivisiblebyFive_true() {
-    PingPong testPingPong = new PingPong();
-    assertEquals(true, pingPong.isPingPong(10))
-  }
-  @Test
-  public void isPingPong_forNumbersNotDivisibleByFive_false() {
-    PingPong testPingPong = new PingPong();
-    assertEquals(false, pingPong.isPingPong(4))
-  }
-  @Test
-  public void isPingPong_forNumbersDivisibleByThreeAndFive_true() {
-    PingPong testPingPong = new PingPong();
-    assertEquals(true, pingPong.isPingPong(15))
-  }
-  @Test
-  public void isPingPong_forNumbersNotDivisibleByThreeAndFive_false() {
-    PingPong testPingPong = new PingPong();
-    assertEquals(false, pingPong.isPingPong(16))
-  }
+  // @Test
+  // public void isPingPong_forNumbersDivisiblebyThree_Array(){
+  //   PingPong testPingPong = new PingPong();
+  //   ArrayList<Object> expected = new ArrayList<Object>();
+  //   expected.add(1);
+  //   expected.add(2);
+  //   expected.add("ping");
+  //   assertEquals(expected, testPingPong.isPingPong(3));
+  // }
+  // @Test
+  // public void isPingPong_forNumbersDivisiblebyFive_Array(){
+  //   PingPong testPingPong = new PingPong();
+  //   ArrayList<Object> expected = new ArrayList<Object>();
+  //   expected.add(1);
+  //   expected.add(2);
+  //   expected.add("ping");
+  //   expected.add(4);
+  //   expected.add("pong");
+  //   assertEquals(expected, testPingPong.isPingPong(5));
+  // }
+  // @Test
+  // public void isPingPong_forNumbersDivisiblebyFifteen_Array(){
+  //   PingPong testPingPong = new PingPong();
+  //   ArrayList<Object> expected = new ArrayList<Object>();
+  //   expected.add(1);
+  //   expected.add(2);
+  //   expected.add("ping");
+  //   expected.add(4);
+  //   expected.add("pong");
+  //   expected.add("ping");
+  //   expected.add(7);
+  //   expected.add(8);
+  //   expected.add("ping");
+  //   expected.add("pong");
+  //   expected.add(11);
+  //   expected.add("ping");
+  //   expected.add(13);
+  //   expected.add(14);
+  //   expected.add("pingpong");
+  //   assertEquals(expected, testPingPong.isPingPong(15));
+  // }
+  // @Test
+  // public void isPingPong_forNumberLessThanThree_true() {
+  //   PingPong testPingPong = new PingPong();
+  //   assertEquals(true, pingPong.isPingPong(2))
+  // }
+  // @Test
+  // public void isPingPong_forNumbersDivisibleByThree_true() {
+  //   PingPong testPingPong = new PingPong();
+  //   assertEquals(true, pingPong.isPingPong(3))
+  // }
+  // @Test
+  // public void isPingPong_forNumbersNotDivisibleByThree_false() {
+  //   PingPong testPingPong = new PingPong();
+  //   assertEquals(false, pingPong.isPingPong(7))
+  // }
+  // @Test
+  // public void isPingPong_forNumbersDivisiblebyFive_true() {
+  //   PingPong testPingPong = new PingPong();
+  //   assertEquals(true, pingPong.isPingPong(10))
+  // }
+  // @Test
+  // public void isPingPong_forNumbersNotDivisibleByFive_false() {
+  //   PingPong testPingPong = new PingPong();
+  //   assertEquals(false, pingPong.isPingPong(4))
+  // }
+  // @Test
+  // public void isPingPong_forNumbersDivisibleByThreeAndFive_true() {
+  //   PingPong testPingPong = new PingPong();
+  //   assertEquals(true, pingPong.isPingPong(15))
+  // }
+  // @Test
+  // public void isPingPong_forNumbersNotDivisibleByThreeAndFive_false() {
+  //   PingPong testPingPong = new PingPong();
+  //   assertEquals(false, pingPong.isPingPong(16))
+  // }
 }
