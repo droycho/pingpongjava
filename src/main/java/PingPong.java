3697505653c35b427c35b229518cb423f17aa3e7
DJ Roychoudhury and Tommy Jones <tommy@>
2016-04-18 16:45:10 -0700

    all tests work, added home.vtl, detector.vtl, and layout.vtl. page works, just need to rewrite to give proper output page.

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -7,7 +7,28 @@ import static spark.Spark.*;
 
 
 public class PingPong {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/detector", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/detector.vtl");
+
+      String index = request.queryParams("index");
+      Integer integerIndex = Integer.parseInt(index);
+      ArrayList<Object> isPingPong = isPingPong(integerIndex);
+
+      model.put("isPingPong", isPingPong);
+      model.put("index", request.queryParams("index"));
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
 
   public static ArrayList<Object> isPingPong(Integer num) {
     ArrayList<Object> pingPongArray = new ArrayList<Object>();
